d28ff87d3ed7bd894022b163640fdc143f2a4cdf
huemac16 <huemac16@htl-kaindorf.ac.at>
2019-03-26 09:44:06 +0100

    added some if statements

--- src/main/java/htl/huebl/consumerproducerpassword/PasswordConsumer.java
+++ src/main/java/htl/huebl/consumerproducerpassword/PasswordConsumer.java
@@ -1,12 +1,28 @@
 package htl.huebl.consumerproducerpassword;
 
+import java.util.ArrayList;
+
 public class PasswordConsumer implements Runnable {
+
+    private ArrayList<Password> passwords = new ArrayList<>();
+
+    public PasswordConsumer(ArrayList<Password> passwords) {
+        this.passwords = passwords;
+
+    }
 
     private Password password = new Password();
 
     @Override
     public void run() {
         while (true) {
+            synchronized (password) {
+                if (!passwords.isEmpty()) {
+                    password = passwords.get(0);
+                    passwords.remove(0);
+                }
+
+            }
 
         }
     }
